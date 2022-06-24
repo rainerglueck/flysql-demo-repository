90f82ce72f71fd504fdc3f6d51a26e9dbe342007
Rainer Glück <rainer.glueck@gmx.ch>
2022-06-24 13:35:41 +0200

    changes some parameters to have special characters

--- sql-scripts/select.3params.dual.sql
+++ sql-scripts/select.3params.dual.sql
@@ -1,3 +1,3 @@
-select '${LINE1}' V from dual union all
+select '${LINE 1}' V from dual union all
 select '${LINE2}' V from dual union all
-select '${LINE3}' V from dual
+select '${LINE3&}' V from dual
